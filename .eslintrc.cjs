427f982a8686ad84727a315a783175128c7c7d8a
Ernesto Herrera <ernerdo@gmail.com>
2023-02-19 18:54:21 -0600

    Add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/export': 0,
+  },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Container } from "@mui/material";
-import Button from "@mui/material/Button";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Container } from '@mui/material'
+import Button from '@mui/material/Button'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <Container maxWidth="sm">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -23,7 +23,7 @@ function App() {
       </p>
       <Button variant="contained">Hello World</Button>
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
